9c0d8f00809d50290b1166e08638787591fdf5c7
Nick Collins <n0488629@actx.edu>
2025-03-23 09:24:00 -0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;  // Import the Scanner class
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in); // create scanner object
+      
+      System.out.print("Enter the length of the rectangle: ");
+      int length = myObj.nextInt(); // read user input
+      System.out.print("Enter the width of the rectangle: ");
+      int width = myObj.nextInt(); // read user input
+      
+
+      int area = length * width; // calculate total area
+      int perimeter = length + length + width + width;
+       System.out.println("Results:");
+       System.out.println("Area of the rectangle: " + area);
+       System.out.println("Perimeter of the rectangle: " + perimeter);
+      myObj.close(); // close scanner object
+    }
+  }
